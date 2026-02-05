d060c9e3dd8b77ba3872e1fb569ead03b541c119
David Alberto Maradiaga Banegas <dmaradiaga08@gmail.com>
2026-02-04 18:56:15 -0600

    Actualizando el main.py

--- app/main.py
+++ app/main.py
@@ -48,4 +48,5 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    # Aumentar el timeout para dar tiempo a que el modelo se descargue de DagshHub
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True, timeout_keep_alive=120)
